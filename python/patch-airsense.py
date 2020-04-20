eaa606d2c94246ea85be8b230562680e0cb06d58
Colin O'Flynn <coflynn@newae.com>
2020-04-19 22:18:36 -0300

    Update patch script, adds uart3 monitor by default

--- python/patch-airsense.py
+++ python/patch-airsense.py
@@ -14,9 +14,12 @@ import crcmod
 import crcmod.predefined
 import os.path
 import struct
+import re
 
 class ASFirmware(object):
     """Patch firmware from device with various changes"""
+
+    reserve_marker = 0xBA
 
     def __init__(self, file):
         self.fw = file.read()
@@ -72,14 +75,14 @@ class ASFirmware(object):
         i2 = bytes(self.fw).rfind(bytes(dataseq))
         
         if i1 != i2:
-            raise ValueError("Passed sequence is not unique! Found at 0x%x and 0x%"%(i1, i2))
+            raise ValueError("Passed sequence is not unique! Found at 0x%x and 0x%x"%(i1, i2))
 
         if i1 == -1:
             raise ValueError("Passed sequence not found")
 
         return i1
 
-    def patch(self, patchdata, addr=None, dataseq=None, hash=None, verbose=True):
+    def patch(self, patchdata, addr=None, dataseq=None, hash=None, verbose=True, checkreserved=True, checkempty=False, clobber=True):
         """Updates firmware data with patchdata, based on address, sequence, or hash of sequence"""
         
         #I love Python3(TM)
@@ -102,6 +105,23 @@ class ASFirmware(object):
 
         if verbose:
             print("Patching %d bytes at 0x%x"%(patchlen, addr))
+
+        #Reservered uses self.reserve_marker to indicate our usage (more obvious when inspecting...)
+        if checkempty:
+            checkreserved = False
+        
+        if clobber:
+            checkreserved = False
+            checkempty = False
+        
+        if checkreserved:
+            if self.fw[addr:(addr+patchlen)] != self.reserve_marker*len(patchdata):
+                raise ValueError("Appears data in section you want me to patch! Bailing out...")
+
+        if checkempty:
+            if self.fw[addr:(addr+patchlen)] != 0xFF*len(patchdata):
+                raise ValueError("Appears data in section you want me to patch! Bailing out...")
+
         self.fw[addr:(addr+patchlen)] = patchdata
 
     def find_flash_room(self, length_needed, start=0x4000, start_mod=0x100, reserve=True):
@@ -139,7 +159,7 @@ class ASFirmware(object):
         
         if reserve:
             print("Reserving %d bytes"%length_needed)
-            self.fw[candidate:(candidate+length_needed)] = [0xBA] * length_needed
+            self.fw[candidate:(candidate+length_needed)] = [self.reserve_marker] * length_needed
         
         return address
         
@@ -186,36 +206,58 @@ class ASFirmware(object):
         f = open(filename, "wb")
         f.write(bytes(self.fw))
         f.close()
+        
+    def prepare_bin(self, filename):
+        """Uses .lst file to find symbols - could use ELF too put requires additional dependancy"""
+        
+        f = open(filename + ".lst", "rb")
+        lst = f.read()
+        f.close()
+        
+        f = open(filename + ".bin", "rb")
+        bin = f.read()
+        f.close()
+        
+        #Find 'start' symbol we assume each file uses
+        addr_offset = re.search(b'\.text:[0-F]{8} start', lst, re.IGNORECASE).group(0)
+        
+        #addr should look like this now - .text:00000000 start        
+        addr_offset = addr_offset.split(b':')[1].split(b' ')[0]
+        addr_offset = int(addr_offset, 16)
+        
+        return addr_offset, bin
+        
+        
 
 def unlock_ui_limits():
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x4fa8)
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x4fc4)
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7eb0)
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7ee8)
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7ecc)
+    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x4fa8, clobber=True)
+    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x4fc4, clobber=True)
+    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7eb0, clobber=True)
+    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7ee8, clobber=True)
+    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7ecc, clobber=True)
 
 
 def extra_debug():
     # set config variable 0xc value to 4 == enable more debugging data on display
     # if you set it to \x0f it will enable four separate display pages of info in sleep report mode
-    asf.patch(b'\x04', 0x84a8)
+    asf.patch(b'\x04', 0x84a8, clobber=True)
 
 def extra_modes():
     # add more mode entries, set config 0x0 mask to all bits high
     # default is 0x3, which only enables mode 1 (CPAP) and 2 (AutoSet)
     # ---> This is the real magic <---
-    asf.patch(b'\xff\xff', 0x8590)
+    asf.patch(b'\xff\xff', 0x8590, clobber=True)
 
 def extra_menu():
     #try enabling extra menu items
-    asf.patch(b'\x01\x20', 0x66470)
+    asf.patch(b'\x01\x20', 0x66470, clobber=True)
 
 # If you want all menu items to always be visible, let this section run
 def all_menu():
     # force status bit 5 always on -- always editable
-    asf.patch(b'\x01\x20', 0x6e502)
+    asf.patch(b'\x01\x20', 0x6e502, clobber=True)
     # force status bit 4 always on -- this makes all the inputs show up, regardless of mode
-    asf.patch(b'\x01\x20', 0x6e4c4)
+    asf.patch(b'\x01\x20', 0x6e4c4, clobber=True)
 
 
 def gui_config ():
@@ -262,6 +304,36 @@ def patch_logos():
     
     asf.patch_image(setting_loc, pallete_addr, pixels_addr, text)
 
+def patch_uart3_monitor():
+
+
+    irq_offset, irq_bin = asf.prepare_bin("../serial_monitor/monitor_irq")
+    
+    #Need to rebuild if location changes - for now just fix it
+    #irq_location = asf.find_flash_room(len(data)*2)
+    #print("Suggest to place at %x"%irq_location)
+    irq_location = 0xC600
+    asf.patch(irq_bin, irq_location, checkempty=True)
+    
+    init_offset, init_bin = asf.prepare_bin("../serial_monitor/monitor_init")
+    if init_offset != 0:
+        raise ValueError("Nonsense - no other function!?")
+    
+    #Init location
+    init_location = asf.find_bytes([0x70, 0xb5, 0x84, 0xb0, 0x04, 0x46, 00, 0xf0])
+    if (init_location != 0xC339A):
+        raise ValueError("oops.... should rebuild this")
+    asf.patch(init_bin, init_location, clobber=True)
+    
+    #Entry is not at start of file sometimes in this file?
+    print("IRQ has offset of 0x%x (dealt with)"%irq_offset)
+    irq_location += irq_offset
+    
+    # IRQ vector - at fixed location 0x080402DC so don't need to worry about
+    # this moving. Address needs to be +1 for normal code jump location.
+    irq_location_packed = struct.pack("<I", 0x08000000 + irq_location + 1)
+    asf.patch(irq_location_packed, 0x402dc, clobber=True)
+
 if __name__ == "__main__":
     b = open("../stm32.bin", "rb")
     asf = ASFirmware(b)
@@ -269,10 +341,10 @@ if __name__ == "__main__":
 
     #TODO - need to do SHA256 test, or modify addresses to be more generic
 
-    asf.patch(b'\xc0\x46', 0xF0)
-    asf.patch(b'HACKED!', 0x17500)
-    asf.patch(b'NOT FOR USE\x00', 0x1A540)
-    asf.patch(b'WARNING! WARNING! Ventilator test firmware: Not for humans!\x00', 0x1B860)
+    asf.patch(b'\xc0\x46', 0xF0, checkempty=True)
+    asf.patch(b'HACKED!', 0x17500, checkempty=True)
+    asf.patch(b'NOT FOR USE\x00', 0x1A540, checkempty=True)
+    asf.patch(b'WARNING! WARNING! Ventilator test firmware: Not for humans!\x00', 0x1B860, checkempty=True)
 
     #patch_code()
     unlock_ui_limits()
@@ -283,6 +355,8 @@ if __name__ == "__main__":
     gui_config()
 
     patch_logos()
+
+    patch_uart3_monitor()
 
     asf.fix_crcs()
     asf.write_output("../stm32-unlocked.bin", overwrite=True)
